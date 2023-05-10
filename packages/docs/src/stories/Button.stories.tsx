74cc7ead214da685c45b31fde7989e592e8d9413
Henrique Olivo Antonio <henriqueolivoantonio@outlook.com>
2023-05-10 04:39:39 -0300

    chore: improve pages token docs

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -1,3 +1,9 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Button, ButtonProps } from "@playnest-ui/react";
+
 export default {
   title: "Form/Button",
-};
+  component: Button,
+} as Meta<ButtonProps>;
+
+export const Primary: StoryObj<ButtonProps> = {};
